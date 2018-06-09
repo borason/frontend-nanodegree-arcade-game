2b51daf4fac57ab69a5ea8c4f707f6500f57c318
Bora Son <btson310@gmail.com>
2018-06-09 11:13:21 -0600

    adds comments to all files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+'use strict';
+
+// Variable declarations
 let player;
 let allEnemies;
-var win = false;
+let win = false;
+
+// DOM selectors
 const modalBox = document.querySelector('.modal-content');
 const modal = document.querySelector('#win-modal');
 const modalButton = document.querySelector('#modal-button');
@@ -8,7 +13,9 @@ const closeButton = document.querySelector('#close-button');
 const restartButton = document.querySelector('#replay-button');
 const bodySelector = document.querySelector('body');
 
+// Function for when player runs into enemy
 const collision = function() {
+  // Flashes red background color upon collision
   bodySelector.style.backgroundColor = 'rgba(255, 0, 0, 0.3)';
   setTimeout(function() {
     bodySelector.style.backgroundColor = '#eef0d8';
@@ -17,41 +24,44 @@ const collision = function() {
     player.y = 405;
 };
 
+// Opens modal upon player reaching water
 const openModal = function() {
   modalBox.classList.add('show-modal');
   modal.classList.add('overlay');
   modal.classList.remove('modal');
 };
 
+// Closes modal when clicking close button or play again button
 const closeModal = function() {
   modalBox.classList.remove('show-modal');
   modal.classList.remove('overlay');
   modal.classList.add('modal');
-  delete player;
   allEnemies = '';
   win = false;
   startGame();
 }
 
+// Starting function for game when page loads or player replays game
 const startGame = function() {
   allEnemies = [];
-
+  // Creates player at starting position
   player = new Player(200, 405);
-
+  // Array that contains vertical position of each enemy lane
   const trafficLane = [60, 145, 228];
 
+// IIFE creates three enemies: one per lane
   let spawnEnemies = function () {
     for (let i = 0; i < 3; i ++) {
       allEnemies.push(new Enemy (0, trafficLane[i]));
     }
   }();
-  // spawnEnemies();
-modalButton.addEventListener('click', openModal);
-closeButton.addEventListener('click', closeModal);
-restartButton.addEventListener('click', closeModal);
+  // Adds event listeners to modal buttons
+  // modalButton.addEventListener('click', openModal);
+  closeButton.addEventListener('click', closeModal);
+  restartButton.addEventListener('click', closeModal);
 }
 
-
+// Super class for players and enemies
 class Character {
   constructor(x, y) {
     this.x = x;
@@ -59,11 +69,18 @@ class Character {
   }
 }
 
+// Render function
+Character.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Enemy class
 class Enemy extends Character {
   constructor(x, y, speed) {
     super(x, y);
     this.width = 100;
     this.height = 100;
+// Assigns random speed to enemy
     this.speed = Math.floor((Math.random() * 300) + 50);
     this.sprite = 'images/enemy-bug.png';
   }
@@ -71,18 +88,16 @@ class Enemy extends Character {
     this.x += this.speed * dt;
     this.reachedEndOfBoard();
   }
+  // Resets enemy to beginning of canvas and assigns new random speed
   reachedEndOfBoard() {
   if (this.x > ctx.canvas.width) {
     this.x = 0;
     this.speed = Math.floor((Math.random() * 300) + 50);
     }
   }
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
 }
 
-// Now write your own player class
+// Player class
 class Player extends Character {
   constructor(x, y) {
     super(x, y);
@@ -90,26 +105,25 @@ class Player extends Character {
     this.height = 100;
     this.sprite = "images/char-boy.png";
   }
-  render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  }
   update() {
     this.collisionDetector();
+    // Determines win condition
     while (this.y < 0 && (win === false)) {
       openModal();
       win = true;
     }
   }
-    collisionDetector() {
-      allEnemies.forEach(function (enemy){
-      if (player.x < enemy.x + 80 &&
-          player.x + 80 > enemy.x &&
-          player.y < enemy.y + 60 &&
-          60 + player.y > enemy.y) {
-        collision();
-      }
-      });
+  // Detects collision between player and
+  collisionDetector() {
+    allEnemies.forEach(function (enemy){
+    if (player.x < enemy.x + 80 &&
+        player.x + 80 > enemy.x &&
+        player.y < enemy.y + 60 &&
+        60 + player.y > enemy.y) {
+      collision();
     }
+    });
+  }
   handleInput(keyCode) {
         switch(keyCode){
         case 'up':
